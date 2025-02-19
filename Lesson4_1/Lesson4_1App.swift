9ef6b737c8d104cd4344ec02e7647a1238286abe
Evgeny Mastepan <phenikomperekt@gmail.com>
2025-02-19 17:13:52 +0700

    4 is begin

---
+++ Lesson4_1/Lesson4_1App.swift
@@ -0,0 +1,17 @@
+//
+//  Lesson4_1App.swift
+//  Lesson4_1
+//
+//  Created by Evgeny Mastepan on 19.02.2025.
+//
+
+import SwiftUI
+
+@main
+struct Lesson4_1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
